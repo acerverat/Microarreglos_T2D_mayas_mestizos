83710ac8562fe7b4bbf4af7eb4e8f1a1b5bb6bb0
Alejandra <ale.azulceleste@gmail.com>
2023-02-15 16:36:15 -0600

    hhh

--- step04_script_assoc_plink.sh
+++ step04_script_assoc_plink.sh
@@ -1,17 +1,24 @@
 # Pruebas de asociacion con los snps escogidos relacionados a la diabetes y los del microarreglo entero
 
+axiom="axiom_annovar_annotated.hg38_multianno.txt"
+
 cd data
 
 # asociacion con snps de diabetes
 plink -bfile m94samples --extract diabetes_related_snp_ids.txt --assoc --make-bed --out m94samples_diabetes_snps
 awk 'NR==1 || $9<0.05 && $10!="NA" {$1=$1; print $0}' OFS="\t" m94samples_diabetes_snps.assoc > m94samples_diabetes_snps_assoc_significant.tsv
 Rscript ../merge.R m94samples_diabetes_snps_assoc_significant.tsv diabetes_related_snps.csv SNP col1 m94samples_diabetes_snps_assoc_significant_annot_tmp.tsv
-Rscript ../merge.R m94samples_diabetes_snps_assoc_significant_annot_tmp.tsv annovar.hg38_multianno.txt SNP Probe_Set_ID m94samples_diabetes_snps_assoc_significant_annot.tsv
+Rscript ../merge.R m94samples_diabetes_snps_assoc_significant_annot_tmp.tsv ${axiom} SNP Probe_Set_ID m94samples_diabetes_snps_assoc_significant_annot.tsv
+
+plink -bfile m94samples --extract diabetes_related_snp_ids.txt --model --make-bed --out m94samples_diabetes_snps_model
+awk '{$1=$1; print $0}' OFS="\t" m94samples_diabetes_snps_model.model | awk -F"\t" 'NR==1 || $10<0.001' > m94samples_diabetes_snps_model_significant.tsv
+Rscript ../merge.R m94samples_diabetes_snps_model_significant.tsv diabetes_related_snps.csv SNP col1 m94samples_diabetes_snps_model_significant_annot_tmp.tsv
+Rscript ../merge.R m94samples_diabetes_snps_model_significant_annot_tmp.tsv ${axiom} SNP Probe_Set_ID m94samples_diabetes_snps_model_significant_annot.tsv
 
 # asociacion con todos los snps
 plink -bfile m94samples --assoc --make-bed --out m94samples_all_snps
 awk 'NR==1 || $9<0.01 && $10!="NA" {$1=$1; print $0}' OFS="\t" m94samples_all_snps.assoc > m94samples_all_snps_assoc_significant.tsv
 Rscript ../merge.R m94samples_all_snps_assoc_significant.tsv all_snps.csv SNP col1 m94samples_all_snps_assoc_significant_annot_tmp.tsv
-Rscript ../merge.R m94samples_all_snps_assoc_significant_annot_tmp.tsv annovar.hg38_multianno.txt SNP Probe_Set_ID m94samples_all_snps_assoc_significant_annot.tsv
+Rscript ../merge.R m94samples_all_snps_assoc_significant_annot_tmp.tsv ${axiom} SNP Probe_Set_ID m94samples_all_snps_assoc_significant_annot.tsv
 
 cd ..
